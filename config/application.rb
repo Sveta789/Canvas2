a9cfd74f2103b2f9bd0e91d786c5d5481f91d8b4
billrozy <you@example.com>
2016-06-29 18:33:51 +0300

    Configured for deployment to Heroku2

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,5 @@ module Canvas2
     config.assets.initialize_on_precompile=false
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-    config.assets.precompile += [
-        'custom.scss'
-    ]
   end
 end
