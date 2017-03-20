0d08223065e0d205b166dfba1890bbf638727ba1
billrozy <you@example.com>
2017-03-20 23:22:47 +0300

    fog cfg

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,18 @@
 CarrierWave.configure do |config|
 
   # Use local storage if in development or test
-  if Rails.env.development? || Rails.env.test?
-    CarrierWave.configure do |config|
-      config.storage = :file
-    end
-  end
-
-  # Use AWS storage if in production
-  if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.storage = :fog
-    end
-  end
+  # if Rails.env.development? || Rails.env.test?
+  #   CarrierWave.configure do |config|
+  #     config.storage = :file
+  #   end
+  # end
+  #
+  # # Use AWS storage if in production
+  # if Rails.env.production?
+  #   CarrierWave.configure do |config|
+  #     config.storage = :fog
+  #   end
+  # end
 
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
