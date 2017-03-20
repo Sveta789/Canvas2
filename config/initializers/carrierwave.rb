ee20a3f2fb12076447809e2132f007e9acde6160
billrozy <you@example.com>
2017-03-20 23:32:38 +0300

    fog cfg

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,29 +1,25 @@
-CarrierWave.configure do |config|
+ # Use local storage if in development or test
+  if Rails.env.development? || Rails.env.test?
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
+  end
 
-  # Use local storage if in development or test
-  # if Rails.env.development? || Rails.env.test?
-  #   CarrierWave.configure do |config|
-  #     config.storage = :file
-  #   end
-  # end
-  #
-  # # Use AWS storage if in production
-  # if Rails.env.production?
-  #   CarrierWave.configure do |config|
-  #     config.storage = :fog
-  #   end
-  # end
-
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     'AKIAIHZACC2XAIUG22EQ',                        # required
-      aws_secret_access_key: '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',                        # required
-      region:                'eu-central-1',                  # optional, defaults to 'us-east-1'
-      # host:                  'http://canvas-storage.s3-website.eu-central-1.amazonaws.com',             # optional, defaults to nil
-      # endpoint:              'http://canvas-storage.s3-website.eu-central-1.amazonaws.com' # optional, defaults to nil
-  }
-  config.fog_directory  = 'canvas-storage'                          # required
-  config.fog_public     = true                                        # optional, defaults to true
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
-end
+  # Use AWS storage if in production
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+      config.fog_provider = 'fog/aws'                        # required
+      config.fog_credentials = {
+          provider:              'AWS',                        # required
+          aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
+          aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required
+          region:                ENV['AWS_REGION'],                  # optional, defaults to 'us-east-1'
+          # host:                  'http://canvas-storage.s3-website.eu-central-1.amazonaws.com',             # optional, defaults to nil
+          # endpoint:              'http://canvas-storage.s3-website.eu-central-1.amazonaws.com' # optional, defaults to nil
+      }
+      config.fog_directory  = 'canvas-storage'                          # required
+      config.fog_public     = true                                        # optional, defaults to true
+      config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+    end
+  end
