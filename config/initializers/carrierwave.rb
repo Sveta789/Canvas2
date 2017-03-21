fe3327b0896f79a789e8233a45163d4c556d2098
billrozy <you@example.com>
2017-03-21 21:38:50 +0300

    fog cfg

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,8 +17,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
       provider:              'AWS',                        # required
-      aws_access_key_id:     'AKIAIHZACC2XAIUG22EQ',                        # required
-      aws_secret_access_key: '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',                        # required
+      aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],                        # required
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],                        # required
       region:                'eu-central-1'                  # optional, defaults to 'us-east-1'
       # host:                  'http://canvas-storage.s3-website.eu-central-1.amazonaws.com',             # optional, defaults to nil
       # endpoint:              'http://canvas-storage.s3-website.eu-central-1.amazonaws.com' # optional, defaults to nil
